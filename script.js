00e13adf94e71f44af161863d4dd67edaa7e2284
Goncalves95 <>
2023-10-15 18:41:56 +0000

    Add a sound players on javascript

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var video = document.getElementById("video1");
     video.play();
 });
+
+function toggleAudio() {
+    var audio = document.getElementById('meuAudio');
+    if (audio.paused) {
+        audio.play();
+        audio.style.display = "block";
+    } else {
+        audio.pause();
+        audio.style.display = "none";
+    }
+}
