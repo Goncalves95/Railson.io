be95f9160e0d6b8329e775a50470b17116f09ae1
Goncalves95 <>
2023-10-31 17:59:49 +0000

    #try 3 to feed intagram on page now with intagram feed helper

--- assets/js/insta.js
+++ assets/js/insta.js
@@ -1,28 +1,36 @@
-// Replace with your Instagram Access Token
-const accessToken = 'GQWROUTZAadFd2WTE2Ry1LZAlpjcEE2dGdxSFFaSmMzNExkOTZAGVW1LRXUtUFY3c3BmWmlVaER1UWJJV1c2ZA1I1Tnk1c19WSVRhNVpkN3BGZA040SF9EcDFlLWdrdmItT2p0LWIzMWo4d0pyQmM4R3JYeUdMNVlveGMZD';
+// Coloque suas credenciais do Instagram aqui
+const CLIENT_ID = '644977577834994';
+const ACCESS_TOKEN = 'IGQWRQMUtkTzZAvZATNTazFvblhjX29YSzZAGT2hqVUFvRE13bjRjU1ZAQYktxNUpoSEpYUjVkR194NzNJWldsRF9RZAmhEUTB0QVJNd003NFFmZAUpia1dFWk1zZAHA5VjM4N1RTNTBHX0dEczNmclBsQ3hyNVZAUMDZABbTAZD';
+const USER_ID = '743207218';
 
-// Function to fetch and display Instagram feed
+// Função para obter os dados do feed do Instagram e exibir as fotos no site
 function getInstagramFeed() {
-    fetch(`https://graph.instagram.com/v12.0/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${accessToken}`)
+    fetch(`https://graph.instagram.com/${USER_ID}/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${ACCESS_TOKEN}`, {
+        method: 'GET',
+    })
         .then(response => response.json())
         .then(data => {
+            // Manipule os dados da resposta aqui
             const feedContainer = document.getElementById('instagram-feed');
-
             data.data.forEach(post => {
-                const postLink = document.createElement('a');
-                postLink.href = post.permalink;
-                postLink.target = '_blank';
-
-                const postImage = document.createElement('img');
-                postImage.src = post.media_url;
-                postImage.alt = post.caption;
-
-                postLink.appendChild(postImage);
-                feedContainer.appendChild(postLink);
+                if (post.media_type === 'IMAGE') {
+                    const img = document.createElement('img');
+                    img.src = post.media_url;
+                    img.alt = post.caption;
+                    feedContainer.appendChild(img);
+                } else if (post.media_type === 'VIDEO') {
+                    const video = document.createElement('video');
+                    video.src = post.media_url;
+                    video.alt = post.caption;
+                    feedContainer.appendChild(video);
+                }
             });
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            // Trate os erros aqui
+            console.error('Ocorreu um erro ao obter o feed do Instagram:', error);
+        });
 }
 
-// Call the function to fetch and display the Instagram feed
-getInstagramFeed();
+// Chame a função para obter o feed do Instagram quando a página for carregada
+document.addEventListener('DOMContentLoaded', getInstagramFeed);
