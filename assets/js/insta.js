d9d26e20722b54cbc2aef6af4542bbc3adfa559b
Goncalves95 <>
2023-10-31 17:05:27 +0000

    change the javascript code feed

--- assets/js/insta.js
+++ assets/js/insta.js
@@ -1,26 +1,28 @@
-$(function () {
+// Replace with your Instagram Access Token
+const accessToken = 'GQWROUTZAadFd2WTE2Ry1LZAlpjcEE2dGdxSFFaSmMzNExkOTZAGVW1LRXUtUFY3c3BmWmlVaER1UWJJV1c2ZA1I1Tnk1c19WSVRhNVpkN3BGZA040SF9EcDFlLWdrdmItT2p0LWIzMWo4d0pyQmM4R3JYeUdMNVlveGMZD';
 
-    const token = "IGQWRNTEloNndrRDhrUF9pNjdsNGV0UkJnQ1Vwc2RORWNZAWVR0Mmt1cGlmWGdZAMXZA1cFhhN1NfVm1rZAVhHdkdaeGtDZAktpY3pTbHNRaUZAkMnVYbk52bTVSMjl6VkRHa2VwdkhiaEUtSGMwVzl4OXEyZAnBEMzR2SmMZD";
-    const url = `https://graph.instagram.com/v12.0/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${token}`;
+// Function to fetch and display Instagram feed
+function getInstagramFeed() {
+    fetch(`https://graph.instagram.com/v12.0/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${accessToken}`)
+        .then(response => response.json())
+        .then(data => {
+            const feedContainer = document.getElementById('instagram-feed');
 
-    $.get(url).then(function (response) {
-        //console.log('retorno: ', response.data);
-        let dadosJson = response.data
-        let conteudo = '<div class="row" style="padding-left:5px">';
+            data.data.forEach(post => {
+                const postLink = document.createElement('a');
+                postLink.href = post.permalink;
+                postLink.target = '_blank';
 
-        for (let p = 0; p < dadosJson.length; p++) {
-            let feed = dadosJson[p];
-            let titulo = feed.caption !== null ? feed.caption : '';
-            let tipo = feed.media_type;
-            if(tipo === 'VIDEO') {
-                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xxl-4"><video style="width:100%;height:90%" controls><source src="'+feed.media_url+'" type="video/mp4"></video></div>';
-            }
-            else if (tipo === 'IMAGE') {
-                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xxl-4"><img style="width:100%;height:90%" title="'+titulo+'" alt"'+titulo+'" src="'+feed.media_url+'" onclick="window.open(\''+feed.permalink+'\');></div>';
-            }
-        }
-        conteudo += '</div>';
-        $('#insta').html(conteudo) 
-    })
+                const postImage = document.createElement('img');
+                postImage.src = post.media_url;
+                postImage.alt = post.caption;
 
-});
+                postLink.appendChild(postImage);
+                feedContainer.appendChild(postLink);
+            });
+        })
+        .catch(error => console.error(error));
+}
+
+// Call the function to fetch and display the Instagram feed
+getInstagramFeed();
