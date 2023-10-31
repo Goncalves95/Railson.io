c2e0d02105eda764a195aefca05b9435b2f4e9d6
Goncalves95 <>
2023-10-31 12:35:19 +0000

    Chang the url token

--- assets/js/insta.js
+++ assets/js/insta.js
@@ -1,7 +1,7 @@
 $(function () {
 
     const token = "IGQWRNTEloNndrRDhrUF9pNjdsNGV0UkJnQ1Vwc2RORWNZAWVR0Mmt1cGlmWGdZAMXZA1cFhhN1NfVm1rZAVhHdkdaeGtDZAktpY3pTbHNRaUZAkMnVYbk52bTVSMjl6VkRHa2VwdkhiaEUtSGMwVzl4OXEyZAnBEMzR2SmMZD";
-    const url = "https://graph.instagram.com/me/media?acess_token=" + token + "&fields=media_url,media_type,caption,permalink";
+    const url = `https://graph.instagram.com/v12.0/me/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${token}`;
 
     $.get(url).then(function (response) {
         //console.log('retorno: ', response.data);
