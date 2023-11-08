a2a7688ecac55e7ab89db09edc795a2b95186351
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2023-11-08 16:29:11 +0100

    fixing the slide pages and the new navbar in bottom of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var video = document.getElementById("video");
     video.play();
 });
+
+(function($) {
+    $(function() {
+      $('.toggle-overlay').click(function() {
+        $('aside').toggleClass('open');
+      });
+    });
+  })(jQuery);
